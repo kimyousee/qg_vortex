49f1ee3e9ffc8c48f8294ce51638c6246cf3a79d
kimyousee <kusi@uwaterloo.ca>
2014-08-13 15:39:33 -0400

    Update read_qg_vortex_io.py

--- read_qg_vortex_io.py
+++ read_qg_vortex_io.py
@@ -1,17 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-# Script to read in data that was made already with qg_vortex_pc_timing.py
+### Script to read in data that was made already with qg_vortex_pc_io.py ###
+
+# Create directory for storing figs/plots
+FigDir = "figs"
+if not os.path.exists(FigDir):
+    os.mkdir(FigDir)
 
 # Read from necessary files
-
 ## Contains: Lr,Lz,f,g,N,Ro,Lv,Lh,Bu,Nr,Nz,nEV
-parms = np.fromfile('InputData') 
+parms = np.fromfile('storage/InputData') 
 ## Contains: [<size of a vector>, kt0, nconv0, kt1,nconv1, ...] 
-nconvData = np.fromfile('nconvData') 
+nconvData = np.fromfile('storage/nconvData') 
 ## Eigenvalues and eigenvectors
-eigVals = np.fromfile('eigVals',dtype=np.complex128)
-eigVecs = np.fromfile('eigVecs',dtype=np.complex128)
+eigVals = np.fromfile('storage/eigVals',dtype=np.complex128)
+eigVecs = np.fromfile('storage/eigVecs',dtype=np.complex128)
 
 size = nconvData[0]
 Nz = parms[10]
@@ -41,7 +46,7 @@ for eSolve in range((len(nconvData)-1)/2):
     countEigVals = 0
     while (countEigVals < eigVals.shape[0] and np.imag(eigVals[countEigVals]) > 1e-11):
         countEigVals+=1
-        
+
     if countEigVals == 0:
         print("No valid eigenvalues have converged")
     print("\nEigenvalues: \n")
@@ -57,7 +62,7 @@ for eSolve in range((len(nconvData)-1)/2):
         fig = plt.figure()
 
         plt.rcParams["axes.titlesize"] = 8
-        rmode = fig.add_subplot(1,2,1)
+        rmode = fig.add_subplot(2,1,1)
         rmode.tick_params(axis='both', labelsize=8)
         plt.contourf(r[1:N2+1]/1e3, z[1:Nz-1]/1e3, psi.real, levels=lvlr)
         rcbar = plt.colorbar()
@@ -69,7 +74,7 @@ for eSolve in range((len(nconvData)-1)/2):
         title = " m = %d  [real] gr  = %.3e" % (kt,omega.imag)
         plt.title(title)
 
-        imode = fig.add_subplot(1,2,2)
+        imode = fig.add_subplot(2,1,2)
         imode.tick_params(axis='both', labelsize=8)
         plt.contourf(r[1:N2+1]/1e3, z[1:Nz-1]/1e3, psi.imag, levels=lvli)
         icbar = plt.colorbar()
@@ -81,7 +86,7 @@ for eSolve in range((len(nconvData)-1)/2):
         title = " m = %d  [imag] gr  = %.3e" % (kt,omega.imag)
         plt.title(title)
 
-        fig = "QG_Vortex_m%d" % i
+        fig = "figs/QG_Vortex_m%d.eps" % i
         plt.savefig(fig, format='eps', dpi=1000)
         plt.show()
 
