b2531f50c070d042a4ea8fd3fc8a64c908b7d938
kimyousee <kusi@uwaterloo.ca>
2014-08-21 10:14:54 -0400

    Update qg_vortex_pc_timing.py

--- qg_vortex_pc_timing.py
+++ qg_vortex_pc_timing.py
@@ -5,17 +5,14 @@ slepc4py.init(sys.argv)
 from petsc4py import PETSc
 from slepc4py import SLEPc
 import numpy as np
-import scipy as sc
 import scipy.sparse as sp
-import matplotlib.pyplot as plt
-import scipy.linalg as spalg
-from scipy.sparse.linalg import eigs
 import time
 
 Print = PETSc.Sys.Print
 rank = PETSc.COMM_WORLD.Get_rank()
-# This version of the code uses petsc
-
+# This version of the code uses petsc and works in parallel
+# It also prints out the times for different parts of the code
+# Run this using -eps_target 4.87e-06,4.3e-07
 def petscKron(A,B):
     dim = A.shape[0]*B.shape[0] # length of resulting matrix
     
@@ -78,44 +75,44 @@ def geometry(Nr,Nz,parms):
     Dz2[0,0:3] = [1,-2,1]/hz**2
     Dz2[Nz-1,Nz-3:Nz] = [1,-2,1]/hz**2
 
-    sp.dia_matrix(Dr); sp.dia_matrix(Dr2)
-    sp.dia_matrix(Dz); sp.dia_matrix(Dz2)
+    Dr = sp.csr_matrix(Dr); Dr2 = sp.csr_matrix(Dr2)
+    Dz = sp.csr_matrix(Dz); Dz2 = sp.csr_matrix(Dz2)
 
     return [Dr,Dr2,r,Dz,Dz2,z]
 
 def build_Lap(r,z,Dr,Dr2,Dz,Dz2,parms):
     Print("\nTime Lap setup")
     tLap1 = time.time()
+
     Bu = parms.Bu
     N  = len(r)
     N2 = N/2-1
     M  = len(z)
 
     # Dirichlet BCs:
-    D1d = sp.dia_matrix(Dr2[1:N2+1, 1:N2+1])
-    D2d = sp.dia_matrix(Dr2[1:N2+1, N-2:N2:-1])
-    E1d = sp.dia_matrix(Dr[ 1:N2+1, 1:N2+1])
-    E2d = sp.dia_matrix(Dr[ 1:N2+1, N-2:N2:-1])
+    D1d = sp.csr_matrix(Dr2[1:N2+1, 1:N2+1])
+    D2d = sp.csr_matrix(Dr2[1:N2+1, N-2:N2:-1])
+    E1d = sp.csr_matrix(Dr[ 1:N2+1, 1:N2+1])
+    E2d = sp.csr_matrix(Dr[ 1:N2+1, N-2:N2:-1])
 
     tLap2 = time.time()
     Print(tLap2-tLap1)
-    
+
     # Neumann BCs in z
     temp = -np.linalg.inv(np.array([ [Dz[0,0],Dz[0,M-1]],[Dz[M-1,0],Dz[M-1,M-1]] ]))
-    BCz1 = np.dot(temp,Dz[ [0,M-1], 1:M-1 ])
-    Dzz  = sp.dia_matrix(Dz2[1:M-1,1:M-1] + np.dot(Dz2[1:M-1,[0,M-1]],BCz1))
+    BCz1 = temp*Dz[ [0,M-1], 1:M-1 ]
+    Dzz  = sp.csr_matrix(Dz2[1:M-1,1:M-1] + Dz2[1:M-1,[0,M-1]]*BCz1)
     
     tLap3 = time.time()
     Print (tLap3-tLap2)
-
+    
     # Laplacian in polar coordinates:  d_rr + 1/r*d_r + Bu*d_zz
     # Note that this does not include the azimumthal derivative
-    R = sp.dia_matrix(np.diag(1/r[1:N2+1]))
+    R = sp.csr_matrix(np.diag(1/r[1:N2+1]))
     Lap = petscKron( (D1d+D2d+R*(E1d+E2d)).todense(), np.eye(M-2) ) + Bu*petscKron(np.eye(N2),Dzz.todense())
-
+        
     tLap4 = time.time()
     Print (tLap4 - tLap3)
-    
     return Lap
 
 def build_AB(m,r,z,Lap,Pr_bar,Qr_bar,parms):
@@ -153,6 +150,11 @@ def build_AB(m,r,z,Lap,Pr_bar,Qr_bar,parms):
     A.diagonalScale(Pr_bP)
     A.setDiagonal(-Qr_bP,addv=True)
 
+    # # Old code
+    # for i in xrange(start,end):
+    #     A2[i,:] = Pr_bar[i]*B[i,:]
+    #     A2[i,i] = Pr_bar[i]*B[i,i] - Qr_bar[i]
+
     A.assemble()
 
     tA = time.time()
@@ -161,6 +163,7 @@ def build_AB(m,r,z,Lap,Pr_bar,Qr_bar,parms):
     return A, B
 
 class parms(object):
+    # Note 'data' file is in this order, and evals is last in the file (12)
     def __init__(self,Lr,Lz,f,g,N,Ro,Lv,Lh,Bu,pr,Nr,Nz):
         self.Lr = Lr # Horizontal scale
         self.Lz = Lz # Vertical scale
@@ -174,30 +177,32 @@ class parms(object):
         self.pr = pr # 'BT' or 'BC'
         self.Nr = Nr
         self.Nz = Nz
+        # self.arr= np.array([Lr,Lz,f,g,N,Ro,Lv,Lh,Bu,pr,Nr,Nz])
 
-def solve_eigensystem(A,B,nEV,cnt,Nz,N2,guess,kt,problem_type=SLEPc.EPS.ProblemType.GNHEP):
+def solve_eigensystem(A,B,nEV,cnt,Nz,N2,guess,kt,nconvAr,ncCnt,problem_type=SLEPc.EPS.ProblemType.GNHEP):
     rank = PETSc.COMM_WORLD.getRank()
     size = PETSc.COMM_WORLD.getSize()
     eigVals = open('eigVals','wb')
     eigVecs = open('eigVecs','wb')
-    
     E = SLEPc.EPS(); E.create(comm=SLEPc.COMM_WORLD)
 
-    # for when we want to use the guessVec file
+    # for when we want to use the guessVec file as a guess
     if setSpace == False:
         guessVecFile = PETSc.Viewer().createBinary('guessVec.bin','r')
-        guessVec = PETSc.Vec().load(guessVecFile)
-        E.setInitialSpace(guessVec)
+        if guessVecFile != None:
+            guessVec = PETSc.Vec().load(guessVecFile)
+            E.setInitialSpace(guessVec)
 
     E.setOperators(A,B)
     E.setDimensions(nEV, PETSc.DECIDE)
     E.setProblemType(problem_type)
     #E.setWhichEigenpairs(SLEPc.EPS.Which.LARGEST_IMAGINARY)
-    E.setTarget(guess)
-    E.setTolerances(1e-5, max_it=100)
+    # E.setTarget(guess)
+    # E.setTolerances(1e-5, max_it=100)
     
     E.setFromOptions()
     #E.view()
+
     Print ("\nE solve time:")
     eT1 = time.time()
     E.solve()
@@ -209,18 +214,25 @@ def solve_eigensystem(A,B,nEV,cnt,Nz,N2,guess,kt,problem_type=SLEPc.EPS.ProblemT
     nconv = E.getConverged()
     if nconv <= nEV: evals = nconv
     else: evals = nEV
-    eigValsnp = np.zeros(evals,dtype=complex)
-    eigVecsnp = np.zeros([evals,vr.getSize()],dtype=complex)
+    eigValsnp = np.zeros(nconv,dtype=complex)
+    eigVecsnp = np.zeros([nconv,vr.getSize()],dtype=complex)
+
+    #store how many eigenvalues converged
+    nconvAr[ncCnt+1] = kt
+    nconvAr[ncCnt+2] = nconv
     
-    Print("\nEigenvalues: ")
-    for i in range(evals):
+    if nconv == 0:
+        eigVals.close();eigVecs.close();data.close()
+        Print("\nNo eigenvalues have converged\n")
+        return
+
+    for i in xrange(nconv):
         eigVal = E.getEigenvalue(i)
         omega  = eigVal*kt
         
         eigValsnp[i] = eigVal # store eigenvalue
-        if rank == 0:
-            eigValsnp[i].tofile(eigVals)
-            
+        # Print(eigVal)
+
         E.getEigenvector(i,vr,vi)
 
         scatter, vrSeq = PETSc.Scatter.toZero(vr)
@@ -228,58 +240,37 @@ def solve_eigensystem(A,B,nEV,cnt,Nz,N2,guess,kt,problem_type=SLEPc.EPS.ProblemT
         sm = PETSc.ScatterMode.FORWARD
         scatter.scatter(vr,vrSeq,im,sm)
         if rank == 0:
-            for j in range(0,vrSeq.getSize()):
-                eigVecsnp[i,j] = vrSeq[j] # + 1j*vi[j]
-                eigVecsnp[i,j].tofile(eigVecs)
+            eigVecsnp[i,:] = vrSeq[:] # + 1j*vi[j]
 
         # when we want to make the guessVec file
         if setSpace == True:
             guessVec = PETSc.Viewer().createBinary('guessVec.bin','w') 
             guessVec(vr)
-
-    eigVals.close();eigVecs.close()
-
     if rank == 0:
-        eigVals = np.fromfile('eigVals',dtype=np.complex128)
-        eigVecs = np.fromfile('eigVecs',dtype=np.complex128)
-        eigVecs = eigVecs.reshape([evals,vr.getSize()])
-
-        ind = (-np.imag(eigVals)).argsort()
-
-        eigVecs = eigVecs[ind,:]
-        eigVals = kt*eigVals[ind]
-
-        countEigVals = 0
-        while np.imag(eigVals[countEigVals]) > 1e-11:
-            countEigVals+=1
-
-        for i in range(countEigVals):
-            print eigVals[i]/kt
-            eigVec1 = eigVecs[i]
-            psi = eigVec1.reshape([Nz-2,N2],order='F')
-            lvl = np.linspace(psi.real.min(),psi.real.max(),20)
-            plt.contourf(r[1:N2+1]/1e3, z[1:Nz-1]/1e3, psi.real, levels=lvl)
-            plt.colorbar()
-            plt.xlabel('r')
-            plt.ylabel('z')
-            plt.title(['   m = ', (kt),
-            '   i = ', (i),
-            '   gr (direct) = ', omega.imag])
-            if i == 0:
-                plt.savefig('QG_Vortex.eps', format='eps', dpi=1000)
-            #plt.show()
-
+        eigValsnp.tofile(eigVals)
+        eigVecsnp.tofile(eigVecs)
+    eigVals.close();eigVecs.close();data.close()
+    
 if __name__ == '__main__':
     opts = PETSc.Options()
     nEV = opts.getInt('nev', 5)
+    setSpace = opts.getBool('setSpace', True) # true = write eigenvec to binary file
+    
     Nr  = opts.getInt('Nr', 41)
     Nz  = opts.getInt('Nz', 20)
-    setSpace = opts.getBool('setSpace', True) # true = write eigenvec to binary file
-
-    ## Specify Physical Parameters
-                 #Lr,Lz,f,   g,   N,              Ro,Lv,Lh, Bu,    pr
-    parms = parms(6, 6, 8e-5,9.81,np.sqrt(5)*1e-3, 1, 1, 1, 0.1**2,'BC',Nr,Nz)
     
+    ## Specify Physical Parameters
+    Lr,Lz,f,g,N,Ro,Lv,Lh,Bu,pr = 6, 6, 8e-5,9.81,np.sqrt(5)*1e-3, 1, 1, 1, 0.1**2,'BC'
+                 #Lr,Lz,f,   g,   N,              Ro,Lv,Lh, Bu,    pr
+    parms = parms(Lr,Lz,f,g,N,Ro,Lv,Lh,Bu,pr,Nr,Nz)
+    parmsArr = np.array([Lr,Lz,f,g,N,Ro,Lv,Lh,Bu,Nr,Nz,nEV]) #note pr is not included in the array/file
+    
+    # Store data in files
+    data = open("InputData", "wb")
+    nconvData = open("nconvData","wb")
+
+    if rank == 0:
+        parmsArr.tofile(data)
 
     M  = Nz/2
     N2 = (Nr-1)/2
@@ -319,13 +310,18 @@ if __name__ == '__main__':
 
     guess = 4.87e-06+4.3e-07*1j
     cnt = 0
+    ncCnt = 0
+    nconvAr = np.zeros(3) # change this depending on length of for loop * 2 (also storing the kt) + 1 (storing length of eigenvector)
+    nconvAr[0] = Lap.getSize()[0]
     for kt in range(2,3): #ktv=1
         # Build A and B for eigen-analysis
 
         A, B = build_AB(kt,r,z,Lap,Pr_b,Qr_b,parms)
-        
-        solve_eigensystem(A,B,nEV,cnt,Nz,N2,guess,kt)
+
+        solve_eigensystem(A,B,nEV,cnt,Nz,N2,guess,kt,nconvAr,ncCnt)
 
         cnt += 1
+        ncCnt += 2
 
-
+    nconvAr.tofile(nconvData)
+    nconvData.close()
